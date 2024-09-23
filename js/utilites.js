f69a3cc6b898df1ea41faf78c231bb986eddab12
Khairul Islam <khairul15-4102@diu.edu.bd>
2024-09-24 01:50:59 +0600

    all text content update

--- js/utilites.js
+++ js/utilites.js
@@ -41,7 +41,7 @@ function donateCalculation(inputField, preBalance, donateName){
   const div = document.createElement('div');
   div.innerHTML = `
     <div class="border p-8 m-6 rounded-xl">
-      <h2 class="text-xl font-bold primary-color mb-2">${inputValue} Taka is ${donateName}</h2>
+      <h2 class="text-xl font-bold primary-color mb-2">${inputValue} Taka is Donated for ${donateName}</h2>
       <p class="secondary-color text-sm">Date : ${donationTime()} GMT +0600 (Bangladesh Standard Time)</p></div>
   `
   document.getElementById('individual-donate-history').appendChild(div);
